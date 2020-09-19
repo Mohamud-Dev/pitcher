6cddafb89c4c87b5626bdc892c00f3481799d001
Vector254 <ochrist7@gmail.com>
2020-09-19 14:19:08 +0300

    create user class

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 app = Flask(__name__)
 
 def create_app():
---
+++ config.py
@@ -0,0 +1,24 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://vector:2547Viki@localhost/pitcher'
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
