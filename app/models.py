d24f82b7a8c5a4a759bf8368735cf490c6d03107
Vector254 <ochrist7@gmail.com>
2020-09-22 13:42:57 +0300

    add pitches class to database

--- app/models.py
+++ app/models.py
@@ -6,6 +6,7 @@ class User(db.Model):
     username = db.Column(db.String(255),unique = True,nullable = False)
     email = db.Column(db.String(255),unique = True,nullable = False)
     password = db.Column(db.String(255),nullable = False)
+    pitches_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
 
     def save(self):
         db.session.add(self)
@@ -18,4 +19,16 @@ class User(db.Model):
 
     def __repr__(self):
         return f'User {self.username}' 
+
+class Pitches(db.Model):
+    __tablename__= 'pitches'
+    id = db.Column(db.Integer,primary_key = True)
+    category = db.Column(db.String(255))
+    pitch = db.Column(db.String(255))
+    time = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'pitches',lazy="dynamic")
+
+     def __repr__(self):
+    def __repr__(self):
+        return f'User {self.name}'
 
--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from flask_script import Manager, Server
 from app import create_app, db
-from app.models import User
+from app.models import User, Pitches
 from flask_migrate import Migrate, MigrateCommand
 
 
@@ -14,7 +14,7 @@ manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User )
+    return dict(app = app,db = db,User = User, Pitches=Pitches )
 
 if __name__ == "__main__":
-    manager.run()
+    manager.run
